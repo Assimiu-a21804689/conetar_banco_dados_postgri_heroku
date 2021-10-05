488ff56feb06dc6b5f30e235a5cb13f66f3f6698
Assimiu-a21804689 <72891896+Assimiu-a21804689@users.noreply.github.com>
2021-10-05 05:25:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php 
     echo ("OLA MUNDO");
+    $host = "ec2-52-1-20-236.compute-1.amazonaws.com";
+    $usernma = "ewkyvtgeowqfjf";
+    $passwoed = "ca14347bd00152ed05d076f7e80b1cb957c7d3105c7a6193d2e0c23f9797334e";
+    $data_base = "dvf1fou4dmq2s";
+    $conecao = "";
+    try {
+        $conecao =
+            new PDO("pgsql:host=$host;port=5432;dbname=$data_base", $usernma,$passwoed, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }catch (PDOException $e){
+        echo $e->getMessage();
+    }
 ?>
