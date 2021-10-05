0476bb901f058fb106f1052a36e43409e2aff75e
Assimiu-a21804689 <72891896+Assimiu-a21804689@users.noreply.github.com>
2021-10-05 05:12:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php ?>
+<?php 
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET"){
+        $nome = $_GET["nome"];
+        $id = $_GET["id"];
+        $query = "insert into pessoa values ($id, '$nome')";
+    }
+?>
