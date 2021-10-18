e01393a3adb96115dfe4755367b0a7912450bd1c
Assimiu-a21804689 <72891896+Assimiu-a21804689@users.noreply.github.com>
2021-10-18 12:39:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                 }
                 
                 if ($imagem != null){
-                    $query = "insert into imagem values($id_imagem, '$imagem')";
+                    $query = "insert into imagem values('$id_imagem', '$imagem')";
                     $conecao->query($query);
                 }
                 
