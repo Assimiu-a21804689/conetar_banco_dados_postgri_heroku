3a969e3fd8ec0e9424edef941f6e71d15fa160b2
Assimiu-a21804689 <72891896+Assimiu-a21804689@users.noreply.github.com>
2021-10-18 13:04:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             if ($pedido == "consultar"){
                 $id = $_GET['id'];
                 if ($id != null){
-                    $query = "select *from imagem where id=".'$id';
+                    $query = "select *from imagem where id=".'ola';
                     $retorno = $conecao->query($query);
                     $consultor = $retorno->fetchAll();
 
