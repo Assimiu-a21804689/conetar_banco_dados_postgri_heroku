cefcbda7d999465bc500070632a6b3cbf17a6ac9
Assimiu-a21804689 <72891896+Assimiu-a21804689@users.noreply.github.com>
2021-10-20 03:32:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,10 @@
     $passwoed = "ca14347bd00152ed05d076f7e80b1cb957c7d3105c7a6193d2e0c23f9797334e";
     $data_base = "dvf1fou4dmq2s";
     $conecao = "";
-    try {
-        $conecao =
-            new PDO("pgsql:host=$host;port=5432;dbname=$data_base", $usernma,$passwoed, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-            
-                $image = "";
-                $past = "update/nome.jpg";
-                file_put_contents($past, base64_decode($image));
-                echo json_encode('response'=>'Image Sucesso');  
-    }catch (PDOException $e){
-        echo $e->getMessage();
-    }
+    $image = "";
+    $past = "update/nome.jpg";
+    file_put_contents($past, base64_decode($image));
+    echo json_encode('response'=>'Image Sucesso');
+    
 
 ?>
